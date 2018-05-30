97d904a419e068baa2f90a820abb6d08bbf9bb8e
Meagan Blais <meaganblais@gmail.com>
2018-05-30 16:00:29 -0400

    adding mock data

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import CommentForm from './CommentForm';
 import CommentList from './CommentList'
+import DATA from './data';
 
 class CommentBox extends Component {
   constructor() {
     super();
+    this.state = { data: [] };
   }
   render() {
     return (
       <div className="container">
         <div className="comment">
           <h2>Comments:</h2>
-          <CommentList />
+          <CommentList data={DATA} />
         </div>
         <div className="form">
           <CommentForm />
