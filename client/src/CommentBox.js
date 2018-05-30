14c0ea7ab6b4b1f87399ccd2553c71f5e28e32c2
Meagan Blais <meaganblais@gmail.com>
2018-05-30 12:26:44 -0400

    setting up main app component and adding additional packages

---
+++ client/src/CommentBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        Hello MERN!
+      </div>
+    )
+  }
+}
+
+export default CommentBox;
